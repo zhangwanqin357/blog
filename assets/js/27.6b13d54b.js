cbc931a0b4c385f9ea2203d221f9c04fe985f50c
zhangwanqin357 <554189257@qq.com>
2018-07-28 10:14:23 +0800

    add es6

---
+++ assets/js/27.6b13d54b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{86:function(e,n,t){"use strict";t.r(n);var s=t(0),i=Object(s.a)({},function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("这是一个组件的demo")])},[],!1,null,null,null);n.default=i.exports}}]);
